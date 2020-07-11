edb6f7b3ba6b65d3a9bbcf336ecbe6b684aaed3f
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-07-11 19:12:19 +0300

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,8 +1,9 @@
 const io = require('socket.io-client');
 const os = require('os-utils'); //https://github.com/oscmejia/os-utils
-const socket = io.connect("http://localhost:8888");
+const socket = io.connect("ws://localhost:8888");
+// const socket = io.connect("wss://localhost", {rejectUnauthorized: false});
 const password = "ghjkjhgt7y8uijk"
-const server_name="qef"
+const server_name="qefef"
 console.log(os.loadavg(1),
 os.loadavg(5),
 os.loadavg(15))
@@ -11,8 +12,8 @@ socket.on('connect', function(){console.log("connected")});
 socket.on('get_data', function(pass){
     if(pass===password){
         os.cpuUsage( function(cpu_usage) {
-            socket.emit('data_from_server',{server_name:server_name,measurements:[os.loadavg(1),os.loadavg(5),os.loadavg(15),1-os.freememPercentage(),{name:Math.random(),cpu:cpu_usage}],password:password});
+            socket.emit('data_from_server',{server_name:server_name,measurements:[os.loadavg(1),os.loadavg(5),os.loadavg(15),{memory:1-os.freememPercentage()},{cpu:cpu_usage}],password:password});
         } )  
     }
 });
-socket.emit('connect_server',password);
+socket.emit('connect_server',[password,server_name]);
