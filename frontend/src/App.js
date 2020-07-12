20195d0f94d655ac64b1e5729b02c17ac4191e59
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-07-12 21:53:52 +0300

    ui update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,174 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {LineChart,Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip,Legend, ResponsiveContainer} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import socket_io_client from "socket.io-client";
 const ENDPOINT = "http://localhost:8888";
 
- 
+
+
 function App() {
-  const history_length=30
+  const history_length = 30
   const [servers_data, setServersData] = React.useState(0);
- 
-  
-  const [offline, setOffline] = React.useState({});
+  const [online, setOnline] = React.useState({});
+  function setOnline_wrapper(server_name, value) {
+    setOnline((online) => {
+      online[server_name] = value
+      return JSON.parse(JSON.stringify(online))
+    })
+  }
   React.useEffect(() => {
     const socket = socket_io_client(ENDPOINT);
     socket.emit("connect_client")
     socket.on("initial_history_data", data => {
       console.log(data)
       setServersData(data.history);
-      setOffline(data.online)
+      setOnline(data.online)
     });
     socket.on("update_history_data", update_data => {
-      const server_name=update_data.server_name
-      setServersData((servers_data)=>{
-        console.log(server_name)
-        console.log(servers_data[server_name])
-        if(servers_data[server_name].cpu_usage.length===history_length){
-          servers_data[server_name].cpu_usage.shift()
-      }
-      if(servers_data[server_name].memory_usage.length===history_length){
-        servers_data[server_name].memory_usage.shift()
-      }
-      servers_data[server_name].avg_load[0]=update_data.data[0]
-      servers_data[server_name].avg_load[1]=update_data.data[1]
-      servers_data[server_name].avg_load[2]=update_data.data[2]
-      servers_data[server_name].memory_usage.push(update_data.data[3])
-      servers_data[server_name].cpu_usage.push(update_data.data[4])
-        console.log(servers_data)
-      
-        return JSON.parse(JSON.stringify(servers_data)) 
-      })
-     
-    });
-    socket.on("offline", server_name => {
-      console.log("offline")
-      setOffline((offline)=>{
-        offline[server_name]=true
-        return offline
+      console.log(update_data)
+      const server_name = update_data.server_name
+      setServersData((servers_data) => {
+        if (servers_data[server_name]) {
+          setOnline_wrapper(server_name, true)
+          if (servers_data[server_name].cpu_usage.length === history_length) {
+            servers_data[server_name].cpu_usage.shift()
+          }
+          if (servers_data[server_name].memory_usage.length === history_length) {
+            servers_data[server_name].memory_usage.shift()
+          }
+          servers_data[server_name].avg_load[0] = update_data.data[0]
+          servers_data[server_name].avg_load[1] = update_data.data[1]
+          servers_data[server_name].avg_load[2] = update_data.data[2]
+          servers_data[server_name].memory_usage.push(update_data.data[3])
+          servers_data[server_name].cpu_usage.push(update_data.data[4])
+          return JSON.parse(JSON.stringify(servers_data))
+        }
+        return servers_data
       })
 
+    });
+    socket.on("offline", server_name => {
+      setOnline_wrapper(server_name, false)
     })
   }, []);
 
-  const data = [
-    {
-      name: 'Page A', uv: 4000
-    },
-    {
-      name: 'Page B', uv: 3000
-    },
-    {
-      name: 'Page C', uv: 2000
-    },
-    {
-      name: 'Page D', uv: 2780
-    },
-    {
-      name: 'Page E', uv: 1890
-    },
-    {
-      name: 'Page F', uv: 2390
-    },
-    {
-      name: 'Page G', uv: 3490
-    },
-  ];
-  console.log(2)
-//   function get_last_measurment(server_name){
-
-//   }
-// //   const listItems = Object.keys(servers_data).map((server_name) =>
-// //   <li key={server_name.toString()}>
-// //       <div>
-// //          <h2>Server {server_name}</h2>
-// // <h3>{(offline[server_name]?"Online":"Offline")}</h3>
-// // <h3>CPU usage: {servers_data[server_name]}</h3>
-// //          <h3>Memory usage: </h3>
-// //          <h3>avg load (5,10,15): </h3>
-// //        </div>
-// //   </li>
-// // );
- let components=Object.keys(servers_data).map((el)=>{
-   console.log(el)
-return (<ServerComponent key={el} name={el}
-status={offline[el]} cpu_usage={servers_data[el].cpu_usage} 
-memory_usage={servers_data[el].memory_usage} avg_load={servers_data[el].avg_load} />)
- })
+  let components = Object.keys(servers_data).map((el) => {
+    return (<ServerComponent key={el} name={el}
+      online={online[el]} cpu_usage={servers_data[el].cpu_usage}
+      memory_usage={servers_data[el].memory_usage} avg_load={servers_data[el].avg_load} />)
+  })
 
   return (
     <div className="App">
-      {servers_data!==0?
-      (components)
-      :null}
-      
-   
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-     
-      {/* <LineChart width={300} height={200} data={servers_data['qef']?.cpu_usage}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis/>
-       <YAxis />
-       <CartesianGrid strokeDasharray="3 3"/>
-       <Tooltip/>
-       <Legend />
-       <Line type="monotone" dataKey="cpu"  stroke="#82ca9d" />
-      </LineChart> */}
-
-      {/* <LineChart width={300} height={200} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis dataKey="name"/>
-       <YAxis/>
-       <CartesianGrid strokeDasharray="3 3"/>
-       <Tooltip/>
-       <Legend />
-       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart> */}
+      {servers_data !== 0 ?
+        (components)
+        : null}
     </div>
   );
 }
 
 function ServerComponent(props) {
-  console.log(props)
   return (
     <div>
-      <h2>Server {props.name}</h2>
-      <h3>{props.status}</h3>
-      <h3>CPU usage: {props.cpu_usage[props.cpu_usage.length-1]?.cpu}</h3>
-      <h3>Memory usage: {props.memory_usage[props.memory_usage.length-1]?.memory}</h3>
-      <h3>avg load (5,10,15): {props.avg_load}</h3>
+      <h2>Server {props.name}: {props.online ? "Online" : "Offline"}</h2>
+      <h3>CPU usage: {props.cpu_usage[props.cpu_usage.length - 1]?.cpu.toFixed(4)}</h3>
+      <h3>Memory usage: {props.memory_usage[props.memory_usage.length - 1]?.memory.toFixed(4)}</h3>
+      <h3>avg load (5,10,15): {props.avg_load[0]?.toFixed(4)} {props.avg_load[1]?.toFixed(4)} {props.avg_load[2]?.toFixed(4)}</h3>
       <LineChart width={300} height={200} data={props.cpu_usage}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      >
         <XAxis />
         <YAxis />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="cpu" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="cpu" dot={false} stroke="#82ca9d" />
       </LineChart>
       <LineChart width={300} height={200} data={props.memory_usage}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      >
         <XAxis />
         <YAxis />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="memory" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="memory" dot={false} stroke="#00f7ff" />
       </LineChart>
     </div>
   )
