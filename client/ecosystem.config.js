a8bd45154ae5dc192dac9cbb20a41aecbeb97836
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-12-03 16:02:16 +0300

    pm2

---
+++ client/ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'status_client',
+  script: './index.js',
+  env: {"NODE_ENV": "production"},
+  log_file: 'log.txt',
+  time: true
+};
---
+++ main_server/ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'status_server',
+  script: './index.js',
+  env: {"NODE_ENV": "production"},
+  log_file: 'log.txt',
+  time: true
+};
